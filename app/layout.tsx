1dd2fc883854f19cf879d9126c3dabdb1734d6cf
chickenpie-three <airell@chickenpie.co>
2026-01-23 13:37:26 +0800

    feat: Add Google Tag Manager (GTM-5BNLQPHW)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Space_Grotesk, Space_Mono } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import CartProvider from '@/components/cart/CartProvider';
 import SmoothScroll from '@/components/providers/SmoothScroll';
@@ -28,7 +29,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5BNLQPHW');`}
+        </Script>
+      </head>
       <body className={`${spaceGrotesk.variable} ${spaceMono.variable} font-body`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5BNLQPHW"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <SmoothScroll>
           <CartProvider>
             {children}
